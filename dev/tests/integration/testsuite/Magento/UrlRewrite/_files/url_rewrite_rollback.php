5efe51dc8ab06ad3327411389b27bd8eaf6c44d0
Alexey Arendarenko <alexeya@ven.com>
2019-11-15 13:54:14 +0200

    UrlRewrite module fixes
    update integration tests
    update fixtures

--- dev/tests/integration/testsuite/Magento/UrlRewrite/_files/url_rewrite_rollback.php
+++ dev/tests/integration/testsuite/Magento/UrlRewrite/_files/url_rewrite_rollback.php
@@ -11,6 +11,16 @@ $registry = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get(\Ma
 $registry->unregister('isSecureArea');
 $registry->register('isSecureArea', true);
 
+/** @var Magento\Cms\Api\PageRepositoryInterface $pageRepository */
+$pageRepository = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get(
+    Magento\Cms\Api\PageRepositoryInterface::class
+);
+$pageRepository->deleteById('page-a');
+$pageRepository->deleteById('page-b');
+$pageRepository->deleteById('page-c');
+$pageRepository->deleteById('page-d');
+$pageRepository->deleteById('page-e');
+
 /** @var \Magento\Catalog\Api\ProductRepositoryInterface $productRepository */
 $productRepository = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
     ->create(\Magento\Catalog\Api\ProductRepositoryInterface::class);
@@ -20,7 +30,7 @@ $urlRewriteCollection = \Magento\TestFramework\Helper\Bootstrap::getObjectManage
     ->create(\Magento\UrlRewrite\Model\ResourceModel\UrlRewriteCollection::class);
 $collection = $urlRewriteCollection
     ->addFieldToFilter('entity_type', 'custom')
-    ->addFieldToFilter('request_path', ['page-a', 'page-b', 'page-c'])
+    ->addFieldToFilter('request_path', ['page-a', 'page-b', 'page-c', 'page-d?param1=1', 'page-e?param1=1'])
     ->addFieldToFilter('entity_id', '333')
     ->load()
     ->walk('delete');
